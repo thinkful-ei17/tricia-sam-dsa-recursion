83a2f821673f498289baad4cf6dcbeb7312beb8c
Sam Gould <sjg326@gmail.com>
2018-02-26 13:24:22 -0800

    starting anagrams challenge

---
+++ anagrams.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function anagrams(word){
+  // Base Case
+  if (iterate === word.length){
+    return;
+  }
+	// Recursive Case
+	for (let i=0; i<word.length; i++){
+		word[0]
+	}
+  anagrams(word.slice(1) + word[0]);
+}
+
+anagrams('play', );
