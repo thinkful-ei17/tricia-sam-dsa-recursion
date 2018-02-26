56368b82f6f477705eb8ec50fd551a67d30adeef
TriciaF <TriciaF@thinkful.com>
2018-02-26 17:11:46 -0500

    completed factorial drill

--- anagrams.js
+++ anagrams.js
@@ -1,15 +1,16 @@
 'use strict';
 
-function anagrams(word){
-  // Base Case
-  if (iterate === word.length){
-    return;
-  }
-	// Recursive Case
-	for (let i=0; i<word.length; i++){
-		word[0]
+function anagrams(word, prefix) {
+	// Base Case
+	if (iterate === word.length) {
+		return;
 	}
-  anagrams(word.slice(1) + word[0]);
+	// Recursive Case
+	for (let i = 0; i < word.length; i++) {
+		word[prefix];
+	}
+	anagrams(word.slice(1) + word[0]);
+	return;
 }
 
-anagrams('play', );
+anagrams('play', 0);
