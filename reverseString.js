aa0f1a5c5c7ee7a24abde8d68f5b5ea793c1d100
Sam Gould <sjg326@gmail.com>
2018-02-26 09:25:09 -0800

    completed reverse string exercise

---
+++ reverseString.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const reverseString = function(str) {
+  if (str.length === 0){
+    return '';
+  }
+  return reverseString(str.slice(1)) + str[0];
+};
+
+console.log(reverseString('string'));
+
+// expected output gnirts
